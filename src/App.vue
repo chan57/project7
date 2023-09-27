f72312309ccb84a355425b7ac7a548f183fdd373
Alexey <dis320852@gmail.com>
2023-09-27 15:33:04 +0300

    navigation

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,40 @@
 <script setup>
 import { computed, reactive } from "@vue/reactivity";
 import { ref } from "vue";
-import TemplaTe from "@components/TemplaTe.vue";
-import CompoNent from "./components/CompoNent.vue";
 
-const valueTemplate = ref("");
-const bo = ref("");
-const color = ref("");
-const setColor = reactive(["blue", "green", "red", "yellow", "black"]);
-let numb = ref("");
-const change = function () {
-  numb = Math.trunc(Math.random() * 5) - 1;
-  color.value = setColor[numb];
-};
-const changeColor = function () {
-  change();
-  if (color.value == setColor[numb]) {
-    change();
-  }
-};
+import Home from './components/Home.vue';
+import About from './components/About.vue';
+import Setting from "./components/Setting.vue"
+
+
+const routes = {
+  '/Home': Home,
+  '/About': About,
+  "/Setting": Setting
+}
+
+const currentPath = ref(window.location.hash)
+
+window.addEventListener('hashchange', () => {
+  currentPath.value = window.location.hash
+})
+
+const currentView = computed(() => {
+  return routes[currentPath.value.slice(1) || '/'] || NotFound
+})
+
 </script>
 
 <template>
-  <div>
-    <TemplaTe @chek="(n) => (bo = n)" />
+ <div>
+<a href="#/Home">Home</a> |
+<a href="#/About">About</a> |
+<a href="#/Setting">Setting</a>
+<component :is="currentView" />
 
-    <div v-show="bo">
-      <h2 :style="{ color: color }">напищить количество</h2>
-      <button @click="changeColor">change color</button>
-      <input v-model.trim.number="valueTemplate" />
-      <button v-on:click="valueTemplate = ''">delete</button>
 
-      <h3>{{ valueTemplate }}</h3>
+</div>
 
-      <div v-show="valueTemplate">
-        <div v-for="i of valueTemplate" v-bind:key="i">
-          <CompoNent />
-        </div>
-      </div>
-    </div>
-  </div>
 </template>
 
 <style>
---
+++ src/components/Setting.vue
@@ -0,0 +1,54 @@
+<script setup>
+import { computed, reactive } from "@vue/reactivity";
+import { ref } from "vue";
+import TemplaTe from "@components/TemplaTe.vue";
+import CompoNent from "@components/CompoNent.vue";
+;
+
+const valueTemplate = ref("");
+const bo = ref("");
+const color = ref("");
+const setColor = reactive(["blue", "green", "red", "yellow", "black"]);
+let numb = ref("");
+const change = function () {
+  numb = Math.trunc(Math.random() * 5) - 1;
+  color.value = setColor[numb];
+};
+const changeColor = function () {
+  change();
+  if (color.value == setColor[numb]) {
+    change();
+  }
+};
+
+
+
+</script>
+
+<template>
+ <div>
+
+  <div class="setting-place">
+    <TemplaTe @chek="(n) => (bo = n)" />
+
+    <div v-show="bo">
+      <h2 :style="{ color: color }">напищить количество</h2>
+      <button @click="changeColor">change color</button>
+      <input v-model.trim.number="valueTemplate" />
+      <button v-on:click="valueTemplate = ''">delete</button>
+
+      <h3>{{ valueTemplate }}</h3>
+
+      <div v-show="valueTemplate">
+        <div v-for="i of valueTemplate" v-bind:key="i">
+          <CompoNent />
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+</template>
+
+<style>
+</style>
