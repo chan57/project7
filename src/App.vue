55319b5a1582798c5f9deead5344131acd703326
Alexey <dis320852@gmail.com>
2023-09-27 15:05:23 +0300

    add function chang color

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
 <script setup>
-import { computed } from "@vue/reactivity";
+import { computed, reactive } from "@vue/reactivity";
 import { ref } from "vue";
 import TemplaTe from "@components/TemplaTe.vue";
 import CompoNent from "./components/CompoNent.vue";
 
 const valueTemplate = ref("");
 const bo = ref("");
+const color = ref("");
+const setColor = reactive(["blue", "green", "red", "yellow", "black"]);
+let numb = ref("");
+const change = function () {
+  numb = Math.trunc(Math.random() * 5) - 1;
+  color.value = setColor[numb];
+};
+const changeColor = function () {
+  change();
+  if (color.value == setColor[numb]) {
+    change();
+  }
+};
 </script>
 
 <template>
@@ -13,7 +26,8 @@ const bo = ref("");
     <TemplaTe @chek="(n) => (bo = n)" />
 
     <div v-show="bo">
-      <h2>напищить количество</h2>
+      <h2 :style="{ color: color }">напищить количество</h2>
+      <button @click="changeColor">change color</button>
       <input v-model.trim.number="valueTemplate" />
       <button v-on:click="valueTemplate = ''">delete</button>
 
