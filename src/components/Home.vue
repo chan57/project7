3816bd0485654dd06fef301968468f979cc7f232
Alexey <dis320852@gmail.com>
2023-10-02 21:01:53 +0300

    version

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,32 @@
 <script setup>
+import { ref } from "vue";
+
 function func(event){
   alert(event)
 }
-
+const text = ref('');
+const uslovie = ref(false);
+const newText = ref('');
+const znchenie = ref(false)
+function send() {
+  uslovie.value = true;
+  newText.value = `new Massage: ${text.value}`
+  if(text.value == '') uslovie.value = false
+}
 </script>
 
 <template>
   <div>
+
     <h1>Home</h1>
-    <button @click.once="func($event)">knopk</button>
+    <br>
+    <button @click="znchenie = !znchenie">write message</button>
+    <div v-if="znchenie">
+    <textarea @keydown.enter="send" v-model.trim="text"></textarea>
+    <div v-if="uslovie">
+      <h3>{{ newText }}</h3>
+    </div>
+    </div>
+
   </div>
 </template>
